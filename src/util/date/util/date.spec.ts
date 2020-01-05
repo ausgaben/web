b52c8db6560f5956957996214e676bec4acf390b
Markus Tacker <m@coderbyheart.com>
2020-01-05 13:37:42 +0100

    test: fix test for date formatter

--- src/util/date/util/date.spec.ts
+++ src/util/date/util/date.spec.ts
@@ -4,10 +4,15 @@ import { date } from './date';
 
 describe('date filter', () => {
   it('should format dates', () => {
-    [[new Date('2018-07-01T17:56:47.921Z'), '1.7.']].map(
-      ([value, formatted]) => {
-        expect(date(value as Date)).toEqual(formatted);
-      }
-    );
+    [
+      [
+        new Date(
+          `${new Date().toISOString().substring(0, 4)}-07-01T17:56:47.921Z`
+        ),
+        '1.7.'
+      ]
+    ].map(([value, formatted]) => {
+      expect(date(value as Date)).toEqual(formatted);
+    });
   });
 });
