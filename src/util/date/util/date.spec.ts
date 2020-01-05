5ecd59700e2c629d5d43a945d1580bd792bc8456
Markus Tacker <m@coderbyheart.com>
2020-01-05 13:38:39 +0100

    test(date): add test for year formatting

--- src/util/date/util/date.spec.ts
+++ src/util/date/util/date.spec.ts
@@ -5,6 +5,7 @@ import { date } from './date';
 describe('date filter', () => {
   it('should format dates', () => {
     [
+      [new Date(`2017-07-01T17:56:47.921Z`), '1.7.2017'],
       [
         new Date(
           `${new Date().toISOString().substring(0, 4)}-07-01T17:56:47.921Z`
