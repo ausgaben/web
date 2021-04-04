2483bba552ab4f04f2402d0fbfb5687a950db306
Markus Tacker <m@coderbyheart.com>
2021-04-04 19:04:30 +0200

    fix: simplify graphql client creation

--- src/Apollo/createClient.ts
+++ src/Apollo/createClient.ts
@@ -1,21 +1,11 @@
 import { ApolloClient } from "@apollo/client";
 import { NormalizedCacheObject } from "@apollo/client";
 import { HttpLink } from "@apollo/client";
-import { concat } from "@apollo/client";
-import { AuthLink, AUTH_TYPE } from "aws-appsync-auth-link/lib/auth-link";
-import {
-  SubscriptionHandshakeLink,
-  CONTROL_EVENTS_KEY,
-} from "aws-appsync-subscription-link/lib/subscription-handshake-link";
-import { NonTerminatingLink } from "aws-appsync-subscription-link/lib/non-terminating-link";
-import { onError } from "@apollo/client/link/error";
-import { split, from } from "@apollo/client";
-import { getMainDefinition } from "apollo-utilities";
-import { OperationDefinitionNode } from "graphql";
-import { ApolloLink, Observable } from "@apollo/client";
+import { ApolloLink } from "@apollo/client";
 import { Auth } from "aws-amplify";
 import { InMemoryCache } from "@apollo/client/core";
 import { persistCache, LocalStorageWrapper } from "apollo3-cache-persist";
+import { AuthLink, AUTH_TYPE, AuthOptions } from "aws-appsync-auth-link";
 
 const cache = new InMemoryCache();
 
@@ -26,23 +16,15 @@ persistCache({
 
 const httpLink = new HttpLink({ uri: process.env.REACT_APP_API_ENDPOINT });
 
+const auth: AuthOptions = {
+  type: AUTH_TYPE.AWS_IAM,
+  credentials: Auth.currentCredentials,
+};
+
 const appSyncAuthLink = new AuthLink({
   url: process.env.REACT_APP_API_ENDPOINT,
   region: process.env.REACT_APP_AWS_REGION,
-  auth: {
-    type: AUTH_TYPE.AWS_IAM,
-    credentials: Auth.currentCredentials,
-  },
-});
-
-const errorLink = onError(({ networkError }) => {
-  if (networkError) {
-    if (/Received status code 403/.test(networkError.message)) {
-      Auth.signOut().then(() => {
-        window.location.reload();
-      });
-    }
-  }
+  auth,
 });
 
 export const createClient = () =>
@@ -59,42 +41,6 @@ export const createClient = () =>
         errorPolicy: "all",
       },
     },
-    link: split(
-      ({ query }) => {
-        const { kind, operation } = getMainDefinition(
-          query
-        ) as OperationDefinitionNode;
-        return kind === "OperationDefinition" && operation === "subscription";
-      },
-      concat(
-        appSyncAuthLink,
-        from([
-          new NonTerminatingLink("controlMessages", {
-            link: new ApolloLink(
-              (operation) =>
-                new Observable<any>((observer) => {
-                  const {
-                    variables: {
-                      [CONTROL_EVENTS_KEY]: controlEvents,
-                      ...variables
-                    },
-                  } = operation;
-
-                  if (typeof controlEvents !== "undefined") {
-                    operation.variables = variables;
-                  }
-
-                  observer.next({ [CONTROL_EVENTS_KEY]: controlEvents });
-
-                  return () => {};
-                })
-            ),
-          }),
-          new NonTerminatingLink("subsInfo", { link: httpLink }),
-          new SubscriptionHandshakeLink("subsInfo"),
-        ])
-      ),
-      concat(errorLink, concat(appSyncAuthLink, httpLink))
-    ),
+    link: ApolloLink.from([appSyncAuthLink, httpLink]),
     cache,
   });
